36a1985ef5ef257dc7380571f846e6e6a4d9dbfa
argenis2021 <argenis@argenis-A33G.(none)>
2014-11-16 19:43:56 -0430

    second commit

---
+++ proyecto1/apps/app1/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Publisher, Editorial, Camisas
+
+# Register your models here.
+
+
+admin.site.register(Publisher)
+admin.site.register(Editorial)
+admin.site.register(Camisas)
